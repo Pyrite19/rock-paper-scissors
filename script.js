1d2b2e001fb804f4ee7cdf0da096a720704a92e0
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 17:23:57 -0500

    Added buttons for player choice, which starts a round.

--- script.js
+++ script.js
@@ -15,86 +15,116 @@ function computerPlay() {
   }
 }
 
-function game() {
-  let humanScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i <= 5; i++) {
-    if (i === 5) {
-      if (humanScore > computerScore) {
-        console.log("You win the game!");
-      }
-      if (humanScore < computerScore) {
-        console.log("You lose the game!");
-      }
-    }
+// function game() {
+//   let humanScore = 0;
+//   let computerScore = 0;
+//   for (let i = 0; i <= 5; i++) {
+//     if (i === 5) {
+//       if (humanScore > computerScore) {
+//         console.log("You win the game!");
+//       }
+//       if (humanScore < computerScore) {
+//         console.log("You lose the game!");
+//       }
+//     }
 
-    if (i != 5) {
-      const userInput = prompt(
-        "Rock, Paper, or Scissors? (Can also use R, P, or S)"
-      );
-      const computerInput = computerPlay();
-      const gameResult = playRound(userInput, computerInput);
-      if (gameResult === -1) {
-        i--;
-      }
-      if (gameResult === 0) {
-        console.log(`You lose! Computer chose ${computerInput}!`);
-        computerScore++;
-        console.log(`Score is ${humanScore}-${computerScore}` );
-      }
-      if (gameResult === 1) {
-        console.log(`You tied! Computer chose ${computerInput}!`);
-        console.log("Restarting round!");
-        i--;
-        console.log(`Score is ${humanScore}-${computerScore}`);
-      }
-      if (gameResult === 2) {
-        console.log(`You win! Computer chose ${computerInput}!`);
-        humanScore++;
-        console.log(`Score is ${humanScore}-${computerScore}`);
-      }
-    }
-  }
-}
+//     if (i != 5) {
+//       const userInput = prompt(
+//         "Rock, Paper, or Scissors? (Can also use R, P, or S)"
+//       );
+//       const computerInput = computerPlay();
+//       const gameResult = playRound(userInput, computerInput);
+//       if (gameResult === -1) {
+//         i--;
+//       }
+//       if (gameResult === 0) {
+//         console.log(`You lose! Computer chose ${computerInput}!`);
+//         computerScore++;
+//         console.log(`Score is ${humanScore}-${computerScore}` );
+//       }
+//       if (gameResult === 1) {
+//         console.log(`You tied! Computer chose ${computerInput}!`);
+//         console.log("Restarting round!");
+//         i--;
+//         console.log(`Score is ${humanScore}-${computerScore}`);
+//       }
+//       if (gameResult === 2) {
+//         console.log(`You win! Computer chose ${computerInput}!`);
+//         humanScore++;
+//         console.log(`Score is ${humanScore}-${computerScore}`);
+//       }
+//     }
+//   }
+// }
 
-function abbreviationResolver(string) {
-  if (string === "R") {
-    return "Rock";
-  }
-  if (string === "P") {
-    return "Paper";
-  }
-  if (string === "S") {
-    return "Scissors";
-  }
-}
+// function abbreviationResolver(string) {
+//   if (string === "R") {
+//     return "Rock";
+//   }
+//   if (string === "P") {
+//     return "Paper";
+//   }
+//   if (string === "S") {
+//     return "Scissors";
+//   }
+// }
 
 function playRound(userInput, computerInput) {
-  userInput = capitalize(userInput);
-  userInput = abbreviationResolver(userInput);
-  if (userInput != "Rock" && userInput != "Paper" && userInput != "Scissors") {
-    console.log("Invalid input!");
-    return -1;
-  }
+  // userInput = capitalize(userInput);
+  // userInput = abbreviationResolver(userInput);
+  // if (userInput != "Rock" && userInput != "Paper" && userInput != "Scissors") {
+  //   console.log("Invalid input!");
+  //   return -1;
+  // }
   if (userInput === computerInput) {
-    return 1;
+    return "You tied this round!";
   }
   if (userInput === "Rock" && computerInput === "Paper") {
-    return 0;
+    return "You lose this round!";
   }
   if (userInput === "Rock" && computerInput === "Scissors") {
-    return 2;
+    return "You win this round!";
   }
   if (userInput === "Paper" && computerInput === "Rock") {
-    return 2;
+    return "You win this round!";
   }
   if (userInput === "Paper" && computerInput === "Scissors") {
-    return 0;
+    return "You lose this round";
   }
   if (userInput === "Scissors" && computerInput === "Rock") {
-    return 0;
+    return "You lose this round";
   }
   if (userInput === "Scissors" && computerInput === "Paper") {
-    return 2;
+    return "You win this round!";
   }
 }
+
+let userInput = "";
+
+const container = document.querySelector(".container");
+const div = document.createElement("div");
+const p = document.createElement("p");
+
+const rockBtn = document.querySelector(".playRock");
+rockBtn.addEventListener("click", (e) => {
+  userInput = "Rock"
+  p.textContent = playRound(userInput, computerPlay());
+  container.appendChild(div);
+  div.appendChild(p);
+});
+
+const paperBtn = document.querySelector(".playPaper");
+paperBtn.addEventListener("click", (e) => {
+  userInput = "Paper"
+  p.textContent = playRound(userInput, computerPlay());
+  container.appendChild(div);
+  div.appendChild(p);
+});
+
+const scissorsBtn = document.querySelector(".playScissors");
+scissorsBtn.addEventListener("click", (e) => {
+  userInput = "Scissors"
+  p.textContent = playRound(userInput, computerPlay());
+  container.appendChild(div);
+  div.appendChild(p);
+});
