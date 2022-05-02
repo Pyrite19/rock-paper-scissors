792ca586b58844247b20b9ae4de8d8ec432bcf67
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 19:03:43 -0500

    Added scores on load, fixed inconsistent spacing.

--- script.js
+++ script.js
@@ -15,66 +15,20 @@ function computerPlay() {
   }
 }
 
-// function game() {
-//   let humanScore = 0;
-//   let computerScore = 0;
-//   for (let i = 0; i <= 5; i++) {
-//     if (i === 5) {
-//       if (humanScore > computerScore) {
-//         console.log("You win the game!");
-//       }
-//       if (humanScore < computerScore) {
-//         console.log("You lose the game!");
-//       }
-//     }
-
-//     if (i != 5) {
-//       const userInput = prompt(
-//         "Rock, Paper, or Scissors? (Can also use R, P, or S)"
-//       );
-//       const computerInput = computerPlay();
-//       const gameResult = playRound(userInput, computerInput);
-//       if (gameResult === -1) {
-//         i--;
-//       }
-//       if (gameResult === 0) {
-//         console.log(`You lose! Computer chose ${computerInput}!`);
-//         computerScore++;
-//         console.log(`Score is ${humanScore}-${computerScore}` );
-//       }
-//       if (gameResult === 1) {
-//         console.log(`You tied! Computer chose ${computerInput}!`);
-//         console.log("Restarting round!");
-//         i--;
-//         console.log(`Score is ${humanScore}-${computerScore}`);
-//       }
-//       if (gameResult === 2) {
-//         console.log(`You win! Computer chose ${computerInput}!`);
-//         humanScore++;
-//         console.log(`Score is ${humanScore}-${computerScore}`);
-//       }
-//     }
-//   }
-// }
-
-// function abbreviationResolver(string) {
-//   if (string === "R") {
-//     return "Rock";
-//   }
-//   if (string === "P") {
-//     return "Paper";
-//   }
-//   if (string === "S") {
-//     return "Scissors";
-//   }
-// }
-
 const spacer = document.querySelector(".spacer");
 const div = document.createElement("div");
 const pResult = document.createElement("p");
 const currentScore = document.createElement("h2");
 let humanScore = 0;
 let computerScore = 0;
+
+window.addEventListener("load", (e) => {
+  currentScore.textContent = `Score is ${humanScore}-${computerScore}`;
+  pResult.textContent = "Click below to start!"
+  div.appendChild(currentScore);
+  div.appendChild(pResult);
+  spacer.appendChild(div);
+});
 
 function playRound(userInput, computerInput) {
   if (userInput === computerInput) {
